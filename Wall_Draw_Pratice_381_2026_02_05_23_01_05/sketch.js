07ba21b4f9c7d8381d15e29169dd78b3808f657c
scriss314 <scriss@ucsc.edu>
2026-02-05 15:02:38 -0800

    Wall Draw practice page

---
+++ Wall_Draw_Pratice_381_2026_02_05_23_01_05/sketch.js
@@ -0,0 +1,17 @@
+
+function setup() {
+  createCanvas(1000, 600);
+}
+
+function draw(){
+  background(400);
+  
+  fill("grey")
+  square(100,200,100);
+  fill("yellow")
+  square(200,200,100)
+  fill("red")
+  square(100,300,100)
+  fill("blue")
+  square(200,300,100)
+}
